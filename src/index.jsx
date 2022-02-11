e3302bcfbe36ed8c65a5d5cd18731d976422754e
G51DHU <gs_sidhu@hotmail.co.uk>
2022-02-11 20:53:46 +0000

    Changed "app.jsx" name to "index.jsx"

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 
-function App(){
+function Index(){
   return(
     <>
       <h1>Welcome to React Vite Micro App!</h1>
@@ -9,5 +9,5 @@ function App(){
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Index />, document.getElementById("root"));
 
